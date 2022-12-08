3d719a9fe637a1d9b4ae984a0f8e03c4e63de75a
fliblip <wickynjy@gmail.com>
2022-12-08 10:31:01 +0300

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,19 @@
 
 h1 = Hero.create(name: Faker::Name.name, super_name: Faker::Creature::Animal.name)
 h2 = Hero.create(name: Faker::Name.name, super_name: Faker::Creature::Animal.name)
+h3 = Hero.create(name: Faker::Name.name, super_name: Faker::Creature::Animal.name)
+h4 = Hero.create(name: Faker::Name.name, super_name: Faker::Creature::Animal.name)
+
+p1 = Power.create(name: Faker::Games::Pokemon.move, description: Faker::Markdown.emphasis)
+p2 = Power.create(name: Faker::Games::Pokemon.move, description: Faker::Markdown.emphasis)
+p3 = Power.create(name: Faker::Games::Pokemon.move, description: Faker::Markdown.emphasis)
+p4 = Power.create(name: Faker::Games::Pokemon.move, description: Faker::Markdown.emphasis)
+
+hp1 = HeroPower.create(strength: "Exceptional", power_id: p1.id, hero_id: h1.id)
+hp2 = HeroPower.create(strength: "Average", power_id: p1.id, hero_id: h2.id)
+hp3 = HeroPower.create(strength: "Exceptional", power_id: p2.id, hero_id: h1.id)
+hp4 = HeroPower.create(strength: "Exceptional", power_id: p1.id, hero_id: h3.id)
+hp5 = HeroPower.create(strength: "Average", power_id: p1.id, hero_id: h4.id)
+hp6 = HeroPower.create(strength: "Moderate", power_id: p4.id, hero_id: h2.id)
+
+
