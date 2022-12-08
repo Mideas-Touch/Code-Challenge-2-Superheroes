4b1249daac38b2310cccd3e7e54fd7a96406b0f4
fliblip <wickynjy@gmail.com>
2022-12-08 11:11:12 +0300

    Added hero controller methods

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,15 @@
 class HerosController < ApplicationController
+    def index
+        render json: Hero.all, only:[:id, :name, :super_name]
+    end
+
+    def show
+        hero = Hero.find(params[:id])
+        if hero
+            render json: hero, include: :powers
+        else
+           render json: {error: "Hero not found"}  ,
+           status: :not_found
+        end
+    end
 end
