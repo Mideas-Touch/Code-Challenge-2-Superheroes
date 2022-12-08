540e5f33d6b03aa1a0c1dc56ed4fb7f00d150764
fliblip <wickynjy@gmail.com>
2022-12-08 10:22:07 +0300

    Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+h1 = Hero.create(name: Faker::Name.name, super_name: Faker::Creature::Animal.name)
+h2 = Hero.create(name: Faker::Name.name, super_name: Faker::Creature::Animal.name)
